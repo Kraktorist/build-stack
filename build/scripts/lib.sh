e27681b7d0ba2ef9dc566d13058bcaebb716d264
yournewgoodname <ens-rostov@yandex.ru>
2022-09-08 00:51:17 +0300

    adding yc to lib.sh

--- build/scripts/lib.sh
+++ build/scripts/lib.sh
@@ -1,9 +1,20 @@
 #!/usr/bin/env bash
 
+function yc_init() {
+  yc config profile create current
+  yc config set token $YC_TOKEN
+  yc config set folder-id $YC_FOLDER_ID
+}
+
+function yc_get_network() {
+  yc_init
+  yc vpc network list --format yaml
+}
+
 function init_backend() {
     # init_backend "network"
     step=$1
-    terraform init \
+    terraform -chdir=/app/terraform init \
         -backend-config="endpoint=storage.yandexcloud.net" \
         -backend-config="bucket=${S3_TF_STATE}" \
         -backend-config="region=ru-central1" \
@@ -18,14 +29,14 @@ function init_backend() {
 function init_step() {
     # init_backend "network"
     step=$1
-    cp "${step}.template" "${step}.tf"
+    cp /app/terraform/${step}.template /app/terraform/${step}.tf
 }
 
 function terraform_plan() {
     step=$1
     init_step $step
     init_backend $step
-    terraform plan
+    terraform -chdir=/app/terraform plan
     rm -rf "${step}.tf"
 }
 
@@ -33,15 +44,15 @@ function terraform_apply() {
     step=$1
     init_step $step
     init_backend $step
-    terraform apply -auto-approve
-    rm -rf "${step}.tf"
+    terraform -chdir=/app/terraform apply -auto-approve
+    rm -rf /app/terraform/${step}.tf
 }
 
 function terraform_destroy() {
     step=$1
     init_step $step
     init_backend $step
-    terraform destroy -auto-approve
+    terraform -chdir=/app/terraform destroy -auto-approve
     rm -rf "${step}.tf"
 }
 
@@ -49,18 +60,25 @@ function terraform_status() {
     step=$1
     init_step $step
     init_backend $step
-    terraform show
+    terraform -chdir=/app/terraform show
     rm -rf "${step}.tf"    
 }
 
+function provision_misc() {
+  ansible-playbook -i ${TF_VAR_ansible_inventory} --become /app/ansible/misc/main.yml 
+}
+
 function provision_gitlab() {
-  ansible-playbook -i ${TF_VAR_ansible_inventory} --become ./ansible/gitlab/main.yml 
+  provision_misc
+  ansible-playbook -i ${TF_VAR_ansible_inventory} --become /app/ansible/gitlab/main.yml 
 }
 
 function provision_repos() {
-  ansible-playbook -i ${TF_VAR_ansible_inventory} --become ./ansible/gitlab/main.yml --tag infrastructure --tag apps
+  provision_misc
+  ansible-playbook -i ${TF_VAR_ansible_inventory} --become /app/ansible/gitlab/main.yml --tag infrastructure --tag apps
 }
 
 function provision_nexus() {
-  ansible-playbook -i ${TF_VAR_ansible_inventory} --become ./ansible/nexus/main.yml
+  provision_misc
+  ansible-playbook -i ${TF_VAR_ansible_inventory} --become /app/ansible/nexus/main.yml
 }
