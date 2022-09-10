c98298731408ca52c2f1131b4405febe6b938a2f
yournewgoodname <ens-rostov@yandex.ru>
2022-09-10 16:45:53 +0300

    terraform network another format

--- build/terraform/modules/yc_network/main.tf
+++ build/terraform/modules/yc_network/main.tf
@@ -5,7 +5,7 @@ resource "yandex_vpc_network" "network" {
 resource "yandex_vpc_subnet" "subnet" {
   for_each       = var.subnets
   network_id     = yandex_vpc_network.network.id
-  name           = each.value.name
-  zone           = each.key
+  name           = each.key
+  zone           = each.value.zone
   v4_cidr_blocks = each.value.subnets
 }
