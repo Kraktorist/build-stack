1ad76ab6d7b43562ea566cb91d83f86ff49a881e
kraktorist <1@1.ru>
2022-10-11 12:17:34 +0300

    infra terraform default router to the internet

--- build/terraform/modules/yc_network/main.tf
+++ build/terraform/modules/yc_network/main.tf
@@ -2,12 +2,26 @@ resource "yandex_vpc_network" "network" {
   name = var.network
 }
 
+resource "yandex_vpc_gateway" "default" {
+  name = "foobar"
+  shared_egress_gateway {}
+}
+
+resource "yandex_vpc_route_table" "routing" {
+  network_id = yandex_vpc_network.network.id
+  static_route {
+    destination_prefix = "0.0.0.0/0"
+    gateway_id         = yandex_vpc_gateway.default.id
+  }
+}
+
 resource "yandex_vpc_subnet" "subnet" {
   for_each       = var.subnets
   network_id     = yandex_vpc_network.network.id
   name           = each.key
   zone           = each.value.zone
   v4_cidr_blocks = each.value.subnets
+  route_table_id = yandex_vpc_route_table.routing.id
 }
 
 resource "yandex_vpc_security_group" "security_group" {
