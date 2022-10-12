87c71cbddcbc7801e9ed5e351eaad36e008ef296
kraktorist <1@1.ru>
2022-10-12 19:56:35 +0300

    infra tf module network with prerequisites created

--- build/terraform/modules/yc_network/main.tf
+++ build/terraform/modules/yc_network/main.tf
@@ -1,32 +1,36 @@
-resource "yandex_vpc_network" "network" {
+data "yandex_vpc_network" "network" {
   name = var.network
 }
 
-resource "yandex_vpc_gateway" "default" {
-  name = "default"
-  shared_egress_gateway {}
-}
+# resource "yandex_vpc_gateway" "default" {
+#   name = "default"
+#   shared_egress_gateway {}
+# }
 
-resource "yandex_vpc_route_table" "routing" {
-  network_id = yandex_vpc_network.network.id
-  static_route {
-    destination_prefix = "0.0.0.0/0"
-    gateway_id         = yandex_vpc_gateway.default.id
-  }
+# resource "yandex_vpc_route_table" "routing" {
+#   network_id = yandex_vpc_network.network.id
+#   static_route {
+#     destination_prefix = "0.0.0.0/0"
+#     gateway_id         = yandex_vpc_gateway.default.id
+#   }
+# }
+
+data "yandex_vpc_route_table" "routing" {
+  name = "routing"
 }
 
 resource "yandex_vpc_subnet" "subnet" {
   for_each       = var.subnets
-  network_id     = yandex_vpc_network.network.id
+  network_id     = data.yandex_vpc_network.network.id
   name           = each.key
   zone           = each.value.zone
   v4_cidr_blocks = each.value.subnets
-  route_table_id = yandex_vpc_route_table.routing.id
+  route_table_id = data.yandex_vpc_route_table.routing.id
 }
 
 resource "yandex_vpc_security_group" "security_group" {
   for_each    = var.security_groups
-  network_id  = yandex_vpc_network.network.id
+  network_id  = data.yandex_vpc_network.network.id
   name        = each.key
   description = ""
 
