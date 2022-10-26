4b0d883f6b76577292372663ffbade711777a253
kraktorist <1@1.ru>
2022-10-26 21:21:16 +0300

    infra builder image CMD

---
+++ build/scripts/entry.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+export YC_SERVICE_ACCOUNT_KEY_FILE=/app/key.json
+echo $API_KEY | base64 -d>${YC_SERVICE_ACCOUNT_KEY_FILE}
+echo $ANSIBLE_VAULT_PASSWORD>.vault
+eval `ssh-agent -s`
+ssh-add ~/ya_key
+export ENV_PATH=/app/infrastructure/envs/${ENV}
+export TF_VAR_config=/app/infrastructure/envs/${ENV}/config.yml
+export TF_VAR_ansible_inventory=/app/infrastructure/envs/${ENV}/inventory.yml
+
+source /app/scripts/lib.sh
