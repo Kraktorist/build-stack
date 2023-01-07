12c85a6ea2b41ca8adf9109b8f611a44285d19ac
kraktorist <1@1.ru>
2023-01-07 12:57:16 +0300

    builder adding YC_STORAGE_ACCESS_KEY variables to entrypoint

--- builder/scripts/entry.sh
+++ builder/scripts/entry.sh
@@ -9,5 +9,7 @@ export ENV_PATH=/app/infrastructure/envs/${ENV}
 export TF_VAR_config=/app/infrastructure/envs/${ENV}/config.yml
 export TF_VAR_ansible_inventory=/app/infrastructure/envs/${ENV}/inventory.yml
 export TF_VAR_ENV=${ENV}
+export YC_STORAGE_ACCESS_KEY=${ACCESS_KEY}
+export YC_STORAGE_SECRET_KEY=${SECRET_KEY}
 
 source /app/scripts/lib.sh
